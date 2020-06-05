b4c2ba38f454cec1bf460159a3b07a8cabc4a363
gnole <olegbouchin@gmail.com>
2020-06-05 17:12:24 +0300

    fix

--- hello_word.cpp
+++ hello_word.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-#include <string> 
-using namespace std;
+#include <string>
 int main()
 {
-string name; 
-cin << name; 
-cout <<name<<  "Hello world";
-return 0;
+  std::string name;  // User name
+  std::cin >> name; // Entering the user name
+  std::cout << "Hello world from " << name
+            << std::endl;  // Output a greeting from the user
+  return 0;
+
 }
