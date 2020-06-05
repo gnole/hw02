258ccf5dbc639f61c0dec294101b06c63f61edb2
gnole <olegbouchin@gmail.com>
2020-06-05 16:21:01 +0300

    programma

--- hello_word.cpp
+++ hello_word.cpp
@@ -2,10 +2,11 @@
 #include <string>
 int main()
 {
-  std::string name;  // User name
-  std::cin >> name; // Entering the user name
-  std::cout << "Hello world from " << name
-            << std::endl;  // Output a greeting from the user
+  using namespace std; 
+  string name;  // User name
+  cin >> name; // Entering the user name
+  cout << "Hello world from " << name
+            << endl;  // Output a greeting from the user
   return 0;
 
 }
